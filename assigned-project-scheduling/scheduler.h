8dbe7a4081dfa199517155f2cfa9cf8dad8bb9bb
Jonathan_Tinti <103079375+yyeternal@users.noreply.github.com>
2025-02-18 21:54:46 -0500

    added metrics to RR

--- assigned-project-scheduling/scheduler.h
+++ assigned-project-scheduling/scheduler.h
@@ -16,13 +16,27 @@ struct job {
     int scheduled;
     // other such neccesities?
     // def need some for metrics...
+    int original_length;  
+    int start_time;      
+    int wait_time;       
+    int last_run;
     struct job *next;
 };
 
 struct metrics {
     float num_jobs;
-    float sum_run_time;
+    float sum_turnaround_time;
     float sum_response_time;
+    float sum_wait_time; 
+    struct job_stats* stats_head;
+};
+
+struct job_stats {
+    int id;
+    int response_time;
+    int turnaround_time;
+    int wait_time;
+    struct job_stats* next;
 };
 
 struct args {
@@ -38,6 +52,10 @@ void copy_job(struct job* dst, struct job* src) {
     dst->length         = src->length;
     dst->scheduled      = src->scheduled;
     dst->next           = src->next;
+    dst->original_length= src->length;  
+    dst->start_time     = -1;    
+    dst->wait_time      = 0;      
+    dst->last_run       = -1;
 }
 /* Environment Setup*/
 struct args* arg_parse(char**);
