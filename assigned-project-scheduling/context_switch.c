ccd32a6a1ce459e8380632a2d47f9d61a1dfa6ab
Tinti <jtintinti@gmail.com>
2025-02-20 15:40:47 -0500

    fixed context switch measurements

--- assigned-project-scheduling/context_switch.c
+++ assigned-project-scheduling/context_switch.c
@@ -1,10 +1,12 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/wait.h>
+#include <sched.h>
 
-#define NUM_ITERATIONS 1000
+#define NUM_ITERATIONS 10000
 #define MESSAGE_SIZE 1
 
 double get_time_usec() {
@@ -14,53 +16,71 @@ double get_time_usec() {
 }
 
 int main() {
-    int pipefd[2];
+    int pipe_p_2_c[2];
+    int pipe_c_2_p[2];
     char message[MESSAGE_SIZE];
     pid_t pid;
-    
+
+    cpu_set_t set;
+
+    CPU_ZERO(&set);    
     // Start timing
     double start_time = get_time_usec();
-    
+
+    if (pipe(pipe_p_2_c) < 0 || pipe(pipe_c_2_p) < 0) {
+        perror("pipe failed");
+        return 1;
+    }
     pid = fork();
     if (pid < 0) {
         perror("fork failed");
         exit(1);
     }
     if (pid == 0) {  
-        close(pipefd[1]);  
-        close(pipefd[0]);  
+        CPU_SET(0, &set);
+        if(sched_setaffinity(pid, sizeof(set), &set) == -1) {
+            perror("sched failed");
+            exit(1);
+        }
+
+        close(pipe_p_2_c[1]);  
+        close(pipe_c_2_p[0]);  
         
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             // Read from parent
-            if (read(pipefd[0], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
+            if (read(pipe_p_2_c[0], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
                 perror("read failed");
                 exit(1);
             }
             
             // Write to parent
-            if (write(pipefd[1], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
+            if (write(pipe_c_2_p[1], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
                 perror("write failed");
                 exit(1);
             }
         }
         
-        close(pipefd[0]);
-        close(pipefd[1]);
+        close(pipe_p_2_c[0]);
+        close(pipe_c_2_p[1]);
         exit(0);
         
-    } else {  // Parent process
-        close(pipefd[0]);  
-        close(pipefd[1]);  
+    } else {  
+        CPU_SET(0, &set);
+        if(sched_setaffinity(pid, sizeof(set), &set) == -1) {
+            perror("sched failed");
+            exit(1);
+        }
+        close(pipe_p_2_c[0]);  
+        close(pipe_c_2_p[1]);  
         
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             // Write to child
-            if (write(pipefd[1], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
+            if (write(pipe_p_2_c[1], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
                 perror("write failed");
                 exit(1);
             }
-            
             // Read from child
-            if (read(pipefd[0], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
+            if (read(pipe_c_2_p[0], message, MESSAGE_SIZE) != MESSAGE_SIZE) {
                 perror("read failed");
                 exit(1);
             }
@@ -75,8 +95,8 @@ int main() {
         
         printf("context switch: %f microseconds\n", time);
         
-        close(pipefd[1]);
-        close(pipefd[0]);
+        close(pipe_p_2_c[1]);  
+        close(pipe_c_2_p[0]);
     }
     
     return 0;
