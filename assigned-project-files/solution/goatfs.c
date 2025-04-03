31c87787f9e05c600b60e34d2fb2ba2e21050230
Tinti <jtintinti@gmail.com>
2025-04-02 22:34:14 -0400

    wremove

--- assigned-project-files/solution/goatfs.c
+++ assigned-project-files/solution/goatfs.c
@@ -190,6 +190,76 @@ ssize_t create(){
     return ERR_CREATE_INODE;
 }
 bool wremove(size_t inumber){
+    // if it has not been mounted yet, can't remove it 
+    if (!_disk->Mounts) {
+        return false;
+    }
+
+    char buffer[BLOCK_SIZE];
+    wread(0, buffer);
+    struct _SuperBlock *sb = (struct _SuperBlock *)malloc(sizeof(struct _SuperBlock));
+    memcpy(sb, buffer, sizeof(struct _SuperBlock));
+
+    int block_index = (inumber / 128) + 1;  // +1 because inode blocks start at block 1
+    int inode_offset = inumber % 128;
+
+    // Check if block_index is valid
+    if (block_index > sb->InodeBlocks) {
+        free(sb);
+        return false;
+    }
+
+    char inode_block[BLOCK_SIZE];
+    wread(block_index, inode_block);
+
+    // Get the inode
+    struct _Inode *inode = (struct _Inode *)malloc(sizeof(struct _Inode));
+    memcpy(inode, inode_block + (inode_offset * sizeof(struct _Inode)), sizeof(struct _Inode));
+
+    if (!inode->Valid) {
+        free(inode);
+        free(sb);
+        return false;
+    }
+
+    // Free all direct blocks
+    for (int i = 0; i < 5; i++) {
+        if (inode->Direct[i] == 0) {
+            break;
+        }
+        char empty_block[BLOCK_SIZE] = {0};
+        wwrite(inode->Direct[i], empty_block);
+        
+        // Reset the direct block reference
+        inode->Direct[i] = 0;
+    }
+
+    if (inode->Indirect != 0) {
+        char indirect_buffer[BLOCK_SIZE];
+        wread(inode->Indirect, indirect_buffer);
+        unsigned int *indirect_blocks = (unsigned int *)indirect_buffer;
+        for (int i = 0; i < BLOCK_SIZE / sizeof(unsigned int); i++) {
+            if (indirect_blocks[i] == 0) {
+                break;
+            }
+            char empty_block[BLOCK_SIZE] = {0};
+            wwrite(indirect_blocks[i], empty_block);
+        }
+        char empty_block[BLOCK_SIZE] = {0};
+        wwrite(inode->Indirect, empty_block);
+        
+        inode->Indirect = 0;
+    }
+
+    inode->Valid = 0;
+    inode->Size = 0;
+    memcpy(inode_block + (inode_offset * sizeof(struct _Inode)), inode, sizeof(struct _Inode));
+    wwrite(block_index, inode_block);
+
+    bitmap[((block_index - 1) * (BLOCK_SIZE / sizeof(struct _Inode)) + inode_offset) / 8] &= ~(1 << (inode_offset % 8));
+    free(inode);
+    free(sb);
+
     return true;
 }
 ssize_t stat(size_t inumber) {
